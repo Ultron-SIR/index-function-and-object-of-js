5637c647b679a56ebdf783b1ee8fd5b146facee8
Ultron-SIR <manishkharel523@gmail.com>
2024-08-19 22:04:52 +0545

    Update function.js

--- function.js
+++ function.js
@@ -6,10 +6,10 @@
         
     }
 
-    const subtract = (x, y) => {
+    const subtract=(x, y) => {
         return x - y;
     }
-    const divide = (x, y) => x / y;
+    const divide = (x, y)=> x / y;
 
     
    const e = add(1, 1);
@@ -21,17 +21,17 @@
     console.log(g);
 
     const h= divide(4, 2);
-    console.loh(h);
+    console.log(h);
     
-    const addOne = (x) => x + 1;
+    const addOne = (x) => x+1;
     const i = addOne(2);
     console.log(i);
 
     
     //Default values
     const addValue=(a, b = 10) => {
-        return a + b;
-    }
+        return a+b;
+    };
 
     console.log(addValue(10)) // Prints 20
     console.log(addValue(10,20)) //prints 30
