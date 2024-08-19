62a10ea39cd90aba66e1d3140db637adced6aa9a
Ultron-SIR <manishkharel523@gmail.com>
2024-08-19 22:10:45 +0545

    Update function.js

--- function.js
+++ function.js
@@ -1,40 +1,157 @@
+let Myname = "manish";
+let age = "22";
+const awesome = "True";
 
-    //Functions
+console.log(Myname);  // Prints "manish"
+console.log(age);     // Prints "22"
+console.log(awesome); // Prints "True"
 
-    function add(x, y){
-        return x + y;
-        
-    }
+Myname = "Manish Kharel 123True";
+age = 22.5;
+Myname = 100;
+console.log(Myname);  // Prints 100 (not "Myname")
 
-    const subtract=(x, y) => {
-        return x - y;
-    }
-    const divide = (x, y)=> x / y;
+let webtechnology;
 
-    
-   const e = add(1, 1);
-    const f = add(1, 2);
-    console.log(e);
-    console.log(f);
+console.log(webtechnology);  // Prints undefined
 
-    const g = subtract(5, 1);
-    console.log(g);
+webtechnology = 900;
+console.log(webtechnology);  // Prints 900
 
-    const h= divide(4, 2);
-    console.log(h);
-    
-    const addOne = (x) => x+1;
-    const i = addOne(2);
-    console.log(i);
+webtechnology = null;
+console.log(webtechnology);  // Prints null
 
-    
-    //Default values
-    const addValue=(a, b = 10) => {
-        return a+b;
-    };
+webtechnology = undefined;
+console.log(webtechnology);  // Prints undefined
 
-    console.log(addValue(10)) // Prints 20
-    console.log(addValue(10,20)) //prints 30
+const a = 100;
+const b = 222;
 
-    console.log(addValue("20",undefined));
-    console.log(addValue("20", null)) ;
+const c = a + b;
+console.log(c); // Prints 322
+const d = b - a;
+console.log(d); // Prints 122
+const e = a * b;
+console.log(e); // Prints 22200
+const f = b / a;
+console.log(f); // Prints 2.22
+const g = a % b;
+console.log(g); // Prints 100
+
+const h = "hello";
+const j = "world";
+const k = h + j;
+console.log(k); // Prints "helloworld"
+
+const i = a + h;
+console.log(i); // Prints "100hello"
+
+const l = undefined + h;
+console.log(l); // Prints "undefinedhello"
+
+console.log(1 + "1"); // Prints "11"
+
+let m = 5;
+m = m + 99;
+console.log(m); // Prints 104
+
+/**
+ * Logical operators
+ */
+
+let n = 10;
+const o = "10";
+const r = n == 0;
+console.log(r); // Prints false
+const s = n === o;
+console.log(s); // Prints false (strict equality, different types)
+
+const t = n !== o;
+console.log(t); // Prints true
+
+const v = 20;
+console.log(n > v);  // Prints false
+console.log(n < v);  // Prints true
+console.log(n >= v); // Prints false
+console.log(n <= v); // Prints true
+
+/**
+ * Logical operators
+ */
+let w = true;
+let x = false;
+const p = w && x;
+const q = w || x;
+console.log(p); // Prints false
+console.log(q); // Prints true
+
+const y = !w;
+console.log(y); // Prints false
+
+console.log(!false);    // Prints true
+console.log(!true);     // Prints false
+console.log(!100);      // Prints false (100 is truthy)
+console.log(!null);     // Prints true (null is falsy)
+console.log(!undefined);// Prints true (undefined is falsy)
+
+/*
+Conditional statements
+*/
+let aa = 100;
+const ab = 50;
+
+// If-Else statements
+if (aa > ab) {
+    console.log("a is greater"); // This will print
+} else {
+    console.log("ab is greater");
+}
+
+// Switch case statements
+aa = ""; // Changed to string for testing
+switch (aa) {
+    case 100:
+        console.log("value is 100");
+        break;
+    case 200:
+        console.log("value is 200");
+        break;
+    default:
+        console.log("value is nothing"); // This will print
+        break;
+}
+
+/**
+ * Arrays 
+ */
+const ac = ["Ram", "Hari", "man"];
+console.log(ac);      // Prints the entire array
+console.log(ac[0]);   // Prints "Ram"
+console.log(ac[1]);   // Prints "Hari"
+
+/**
+ * Loops
+ */
+
+// For loop
+for (let i = 0; i < ac.length; i++) {
+    console.log(i);      // Prints index
+    console.log(ac[i]);  // Prints corresponding array element
+}
+
+// While loop
+let ad = 1;
+while (ad < 10) {
+    console.log("the value of ad is");
+    console.log(ad); // Prints the value of ad from 1 to 9
+    ad++;
+}
+
+// Do-while loop
+let ae = 1;
+do {
+    console.log("value of ae is");
+    console.log(ae); // This will print only once because ae is initially 1 and the condition fails after increment
+    ae++;
+} while (ae < 1 && ae < 10);
+
